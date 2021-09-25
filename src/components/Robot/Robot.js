c8e4cc49a3ee202d0b04ee4213708010364733ae
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-26 01:48:02 +0600

    updated Robot component

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -1,15 +1,25 @@
 import React from "react";
+import "./Robot.css";
 
 const Robot = (props) => {
   const { avatar, call_code, made, name, value, version } = props.robot;
   return (
     <div className="robot">
       <img src={avatar} alt={call_code} />
-      <p className="robo-name">{name}</p>
-      <p className="robo-code">{call_code}</p>
-      <p className="robo-company">Made by: {made}</p>
-      <p className="robo-value">Value: {value}</p>
-      <p className="robo-version">Version: {version}</p>
+      <p className="robo-name">{name.slice(0, 10)}</p>
+      <p className="robo-code">
+        Code Name: <span>{call_code.slice(0, 10)}</span>
+      </p>
+      <p className="robo-company">
+        Made by: <span>{made.slice(0, 10)}</span>
+      </p>
+      <p className="robo-value">
+        Value: <span>${value}</span>
+      </p>
+      <p className="robo-version">
+        Version: <span>{version}</span>
+      </p>
+      <button>Purchase</button>
     </div>
   );
 };
