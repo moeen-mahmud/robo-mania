3e0d867c7dbd824cacb2af0e4990b8e428720cfe
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-26 00:58:06 +0600

    fixed header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header-area">
-      <h1>Choose your Robots!</h1>
+      <h1>Choose your Robots 🤖</h1>
       <p className="main-subtitle">Be an awesome robot manager</p>
       <p>Your initial budget is $1 Million!</p>
       <p className="secondary-subtitle">Keep selecting untill the funds end</p>
