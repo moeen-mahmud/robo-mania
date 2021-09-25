9f8bab3ff6d3cac88895ed893559e12bce6d38f5
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-26 03:31:58 +0600

    re-arranged Robot component

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -8,21 +8,22 @@ const Robot = (props) => {
     <div className="robot">
       <img src={avatar} alt={call_code} />
       <p className="robo-name">{name.slice(0, 10)}</p>
-      <p className="robo-code">
-        Code Name: <span>{call_code.slice(0, 10)}</span>
-      </p>
-      <p className="robo-company">
-        Made by: <span>{made.slice(0, 10)}</span>
-      </p>
-      <p className="robo-version">
-        Version: <span>{version}</span>
-      </p>
-      <p className="robo-address">
-        Address: <span>{address}</span>
-      </p>
-      <p className="robo-value">
-        Value: <span>${value}</span>
-      </p>
+      <div className="robo-info">
+        <div>
+          <p className="robo-code">Code Name:</p>
+          <p className="robo-company">Made by:</p>
+          <p className="robo-version">Version:</p>
+          <p className="robo-address">Address:</p>
+          <p className="robo-value">Value:</p>
+        </div>
+        <div>
+          <p>{call_code.slice(0, 10)}</p>
+          <p>{made.slice(0, 10)}</p>
+          <p>{version}</p>
+          <p>{address}</p>
+          <p>${value}</p>
+        </div>
+      </div>
       <button onClick={() => props.handleClick(props.robot)}>Purchase</button>
     </div>
   );
